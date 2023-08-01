d8acc664dc86fcc53e0371c2ac5a8caee9263ec8
Adam Merjil <AMerjil@hotmail.com>
2023-07-31 21:15:21 -0700

    update index useeffect

--- src/pages/index-page.js
+++ src/pages/index-page.js
@@ -1,16 +1,37 @@
 import {useEffect, useState} from "react";
+import axios from 'axios';
 
 import Post from "../pages/post-1";
 
 export default function IndexPage() {
   const [posts,setPosts] = useState([]);
-  useEffect(() => {
-    fetch('https://vidyanews-2dd3196bd5f5.herokuapp.com/post').then(response => {
-      response.json().then(posts => {
-        setPosts(posts);
+
+  // useEffect(() => {
+  //   fetch('https://vidyanews-2dd3196bd5f5.herokuapp.com/post',
+  //   {
+  //     credentials: 'include'
+  //   }
+  //   )
+  //   .then(response => {
+  //     console.log(response)
+  //     response.json()
+  //   .then(posts => {
+  //       setPosts(posts);
+  //     });
+  //   });
+  // }, []);
+  const getPosts = () => {
+    axios.get('https://vidyanews-2dd3196bd5f5.herokuapp.com/post')
+    .then(response => {
+
+        setPosts(response.data);
       });
-    });
+  }
+
+  useEffect(() => {
+   getPosts()
   }, []);
+
   return (
     <div>
       {posts.length > 0 && posts.map(post => (
